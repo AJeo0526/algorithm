21e31958895c9446e8d3f95b9aee4ebe44858f3a
AJeo <AJeo0526@gmail.com>
2019-02-05 15:48:39 +0800

    classic kmp

---
+++ KMP.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+// the space of next has been allocated
+void calNext(char *s, int *next) {
+    int len = strlen(s);
+    // initial
+    int i = 0;
+    next[0] = -1;
+    int k = -1;
+    while (i < len - 1) {
+        // k == -1 implicit that there is no such string
+        if (k == -1 || s[i] == s[k]) {
+            ++i;
+            ++k;
+            next[i] = k;
+        } else {
+            k = next[k];
+        }
+    }
+}
+int KMP(char *target, char *pattern) {
+    int tarLen = strlen(target), patLen = strlen(pattern);
+    int *next = new int[patLen];
+    // initial next
+    calNext(pattern, next);
+    int i = 0, j = 0;
+    int ans = -1;
+    while (i < tarLen) {
+        if (j == -1 || target[i] == pattern[j]) {
+            i++;
+            j++;
+        } else {
+            j = next[j];
+        }
+        if(j == patLen) {
+            ans = i - patLen;
+            break;
+        }
+    }
+    delete[] next;
+    return ans;
+}
+int main() {
+    char tar[] = "hello", pat[] = "llo";
+    cout << KMP(tar, pat) << endl;
+    return 0;
+}
