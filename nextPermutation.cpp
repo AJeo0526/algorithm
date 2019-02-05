3212b6aa4ee645efb92a85a9c5879a95464c2c45
AJeo <AJeo0526@gmail.com>
2019-02-05 09:48:23 +0800

    allow duplicate characters

--- nextPermutation.cpp
+++ nextPermutation.cpp
@@ -11,16 +11,18 @@ void reverseString(char *s, int begin, int end) {
 }
 bool nextPermutation(char *s, int len) {
     int i = len - 2;
-    while (i >= 0 && s[i] > s[i + 1]) i--;
+    // 寻找绝对小于的
+    while (i >= 0 && s[i] >= s[i + 1]) i--;
     if (i < 0) return false;
     int j = len - 1;
-    while (s[j] < s[i]) j--;
+    // 寻找绝对大于的
+    while (s[j] <= s[i]) j--;
     swap(s[i],s[j]);
     reverseString(s, i + 1, len - 1);
     return true;
 }
 int main() {
-    char s[] = "123";
+    char s[] = "1123";
     // you should insure s is increasing sequence
     do {
         cout << s << endl;
